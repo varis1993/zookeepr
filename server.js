0e8dbd7ff3c49e24d0af3dcf45a6b05f97cb16ee
Tavaris Thompson <varis93@yahoo.com>
2022-01-04 11:27:44 -0500

    js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+  app.use(express.static('public'));
   // parse incoming string or array data
   app.use(express.urlencoded({ extended: true }));
   // parse incoming JSON data
@@ -118,4 +119,20 @@ app.get('/api/animals', (req, res) => {
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
+  });
+  
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
